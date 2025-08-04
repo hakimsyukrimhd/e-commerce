517ee6a041129e84667fb261c3e2210f2848a24a
Hakim Syukri <hakimsyukri.mhd@gmail.com>
2025-08-04 09:17:23 +0700

    start again

---
+++ march/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const homePage = require("./routes/homepage");
+
+app.use(express.json);
+
+// main route
+
+app.use("/", homePage);
+
+app.listen(PORT, () => {
+  console.log(`This app running on PORT ${PORT}`);
+});
